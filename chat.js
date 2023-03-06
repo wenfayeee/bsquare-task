22453c9a956a92a3c208f28bddc0956fe06d1b58
Muhammad Hamza <120543468+hamza-deriv@users.noreply.github.com>
2023-03-06 18:31:02 +0800

    task v1.4

--- chat.js
+++ chat.js
@@ -6,11 +6,6 @@ socket.addEventListener('open', () => {
 
 socket.addEventListener('message', (event) => {
   const message = event.data;
-  const chatWindow = document.getElementById('chat-window');
-  const newMessage = document.createElement('div');
-  newMessage.innerText = message;
-  chatWindow.appendChild(newMessage);
-  chatWindow.scrollTop = chatWindow.scrollHeight;
 });
 
 socket.addEventListener('close', () => {
@@ -21,7 +16,6 @@ const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
 
 messageForm.addEventListener('submit', (event) => {
-  event.preventDefault();
   const message = messageInput.value;
   socket.send(message);
   messageInput.value = '';
