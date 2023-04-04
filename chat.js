74f5d7ccccfef3b1c17316067ab21989ed7335c8
wenfayeee <munnfayee@gmail.com>
2023-04-04 10:38:27 +0800

    fixed UI

--- chat.js
+++ chat.js
@@ -1,4 +1,5 @@
 const socket = new WebSocket('ws://localhost:8080');
+const chatWindow = document.getElementById('chat-window');
 
 socket.addEventListener('open', () => {
   socket.send('New user joined the chat');
@@ -6,16 +7,30 @@ socket.addEventListener('open', () => {
 
 socket.addEventListener('message', (event) => {
   const message = event.data;
+  const messageElement = document.createElement('div');
+  console.log(message);
+  
+  //handle asynchronous behaviour with promise using then()
+  message.text().then((data) => {
+    messageElement.innerText = data;
+    chatWindow.appendChild(messageElement);
+  });
 });
 
 socket.addEventListener('close', () => {
+  const messageElement = document.createElement('div');
   // update UI to indicate user has left the chat
+  messageElement.innerText('User has left the chat');
+  chatWindow.appendChild(messageElement);
+  console.log('closed');
+  // socket.send('User has left the chat');
 });
 
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
 
 messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   const message = messageInput.value;
   socket.send(message);
   messageInput.value = '';
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ wss.on('connection', (socket) => {
   console.log('New client connected Server');
 
   socket.on('message', (message) => {
-    console.log(`Received message: ${message}`);
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
